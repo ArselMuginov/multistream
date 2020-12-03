d42e649753f1e90f033950c861ad246faa7a486a
Arsel Muginov <arsel.muginov@gmail.com>
2020-12-04 04:21:28 +0500

    Add removal of channels, style changes

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-var chList = document.querySelector('.ch-list');
+var chList = document.getElementById('ch-list');
 var chListAdd = document.getElementById('ch-list-add');
-var chCount = 0;
 
 chListAdd.onclick = function () {
-    ++chCount;
-
-    var chNum = document.createElement('span');
-    chNum.className = 'ch-num';
-    chNum.textContent = chCount;
-    chList.appendChild(chNum);
+    var chNumber = document.createElement('span');
+    chNumber.className = 'ch-num symbol color1';
+    chNumber.textContent = chList.querySelectorAll('.ch-num').length + 1;
+    chList.insertBefore(chNumber, chListAdd);
 
     var chEdit = document.createElement('span');
-    chEdit.className = 'ch-edit';
+    chEdit.className = 'color2 ch-edit';
     chEdit.contentEditable = true;
-    chList.appendChild(chEdit);
+    chList.insertBefore(chEdit, chListAdd);
 
     var chRemove = document.createElement('button');
+    chRemove.className = 'symbol color-danger';
     chRemove.type = 'button';
     chRemove.textContent = 'X';
-    chList.appendChild(chRemove);
+    chRemove.onclick = function () {
+        chNumber.remove();
+        chEdit.remove();
+        chRemove.remove();
+
+        var chNumbers = chList.querySelectorAll('.ch-num');
+        for (i = 0; i < chNumbers.length; ++i) {
+            chNumbers[i].textContent = i + 1;
+        }
+    }
+    chList.insertBefore(chRemove, chListAdd);
+
+    chEdit.focus();
 }
