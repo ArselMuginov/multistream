cd2025dde06fc3880d7748ff1c5ca7dcd401ffb6
Arsel Muginov <arsel.muginov@gmail.com>
2020-12-04 21:55:14 +0500

    Add styles, icons, tooltips

--- script.js
+++ script.js
@@ -1,32 +1,94 @@
-var chList = document.getElementById('ch-list');
-var chListAdd = document.getElementById('ch-list-add');
+// Dark theme
+document.getElementById('theme-switch').addEventListener('click', function () {
+    document.body.classList.toggle('dark-theme');
+});
 
-chListAdd.onclick = function () {
-    var chNumber = document.createElement('span');
-    chNumber.className = 'ch-num symbol color1';
-    chNumber.textContent = chList.querySelectorAll('.ch-num').length + 1;
-    chList.insertBefore(chNumber, chListAdd);
+// Channel list
+(function () {
+    var channelList = document.getElementById('ch-list');
+    var addBtn = document.getElementById('ch-list-add');
+    var saveBtn = document.getElementById('ch-list-save');
+    var loadBtn = document.getElementById('ch-list-load');
+    var exportBtn = document.getElementById('ch-list-export');
+    var importBtn = document.getElementById('ch-list-import');
+    var clearBtn = document.getElementById('ch-list-clear');
 
-    var chEdit = document.createElement('span');
-    chEdit.className = 'color2 ch-edit';
-    chEdit.contentEditable = true;
-    chList.insertBefore(chEdit, chListAdd);
-
-    var chRemove = document.createElement('button');
-    chRemove.className = 'symbol color-danger';
-    chRemove.type = 'button';
-    chRemove.textContent = 'X';
-    chRemove.onclick = function () {
-        chNumber.remove();
-        chEdit.remove();
-        chRemove.remove();
-
-        var chNumbers = chList.querySelectorAll('.ch-num');
-        for (i = 0; i < chNumbers.length; ++i) {
-            chNumbers[i].textContent = i + 1;
-        }
+    function validateInputs() {
+        channelList.querySelectorAll('input').forEach(input => {
+            var message = input.validationMessage;
+            if (message) {
+                tippy(input, {
+                    content: message,
+                    showOnCreate: true,
+                    trigger: 'manual',
+                    onCreate(instance) {
+                        window.setTimeout(function () {
+                            if (instance) {
+                                instance.destroy();
+                            }
+                        }, 5000);
+                    },
+                    onClickOutside(instance, event) {
+                        instance.destroy();
+                    }
+                });
+            }
+        });
     }
-    chList.insertBefore(chRemove, chListAdd);
 
-    chEdit.focus();
-}
+    // Add new channel to list
+    addBtn.addEventListener('click', function () {
+        var number = document.createElement('span');
+        number.className = 'clr3';
+        number.textContent = channelList.getElementsByTagName('span').length + 1;
+        channelList.appendChild(number);
+
+        var input = document.createElement('input');
+        input.type = 'url';
+        input.placeholder = 'Add channel name...';
+        channelList.appendChild(input);
+
+        var deleteButton = document.createElement('button');
+        deleteButton.className = 'danger';
+        deleteButton.type = 'button';
+        deleteButton.textContent = '\u2715';
+        deleteButton.onclick = function () {
+            number.remove();
+            input.remove();
+            deleteButton.remove();
+
+            var chNumbers = channelList.getElementsByTagName('span');
+            for (i = 0; i < chNumbers.length; ++i) {
+                chNumbers[i].textContent = i + 1;
+            }
+        }
+        channelList.appendChild(deleteButton);
+
+        input.focus();
+    });
+
+    // Local storage save
+    saveBtn.addEventListener('click', function () {
+        validateInputs();
+    });
+
+    loadBtn.addEventListener('click', function () {
+        // local storage save
+    });
+
+    exportBtn.addEventListener('click', function () {
+        // local storage save
+    });
+
+    importBtn.addEventListener('click', function () {
+        // local storage save
+    });
+
+    clearBtn.addEventListener('click', function () {
+        if (window.confirm('Are you sure? Whole channel list will be deleted.')) {
+            while (channelList.firstChild) {
+                channelList.removeChild(channelList.lastChild);
+            }
+        }
+    });
+}());
